bdd7e137df5b4bc6046ab6305ff95f0ccfeadb22
ngiengkianyew <kianyewn@gmail.com>
2025-05-04 22:21:12 +0800

    fix: movies.py config

--- src/data_processing/movies.py
+++ src/data_processing/movies.py
@@ -1,23 +1,25 @@
 import pandas as pd
 
-from configs.model_config import ModelConfig
+from configs.processing_config import DataProcessingConfig
 from src.utils.utils import timeit
 
 
 class ProcessMovies:
-    def __init__(self, movies_df: pd.DataFrame, model_config: ModelConfig):
+    def __init__(
+        self, movies_df: pd.DataFrame, processing_config: DataProcessingConfig
+    ):
         self.movies_df = movies_df
-        self.model_config = model_config
+        self.processing_config = processing_config
         self.output = None
 
     @timeit
     def process_for_training(self):
         self.movies_df["year"] = self.movies_df["title"].str.extract(r"\((\d{4})\)")
-        self.output = self.movies_df[self.model_config.data_movie_features]
+        self.output = self.movies_df[self.processing_config.data_movie_features]
         return self
 
     @timeit
     def process_for_inference(self):
         self.movies_df["year"] = self.movies_df["title"].str.extract(r"\((\d{4})\)")
-        self.output = self.movies_df[self.model_config.data_movie_features]
+        self.output = self.movies_df[self.processing_config.data_movie_features]
         return self
